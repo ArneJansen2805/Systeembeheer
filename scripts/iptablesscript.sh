825267b2f55e32e4a868c41f6cd3e93323af1f24
root <root@arne-jansen.sb.uclllabs.be>
2020-10-21 15:53:41 +0200

    change to fail 2 ban jail

--- scripts/iptablesscript.sh
+++
@@ -1,5 +0,0 @@
-iptables -A INPUT -p tcp -m tcp --dport 22345 -m tcp -m state --state NEW -m recent --set --name SSH --rsource
-
-iptables -A INPUT -p tcp -m tcp --dport 22345 -m recent --update --seconds 120 --hitcount 5 --rttl --name SSH --rsource -j REJECT --reject-with icmp-host-prohibited
-
-iptables -A INPUT -p tcp -m tcp --dport 22345 -j ACCEPT
